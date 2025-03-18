d968993c90990dba6681f987c62305f377a7882a
GenevLG <1234486@cegepba.qc.ca>
2025-03-18 13:37:11 -0400

    Update code

--- wfa_scolaireDepart/ajouterCoursForm.cs
+++ wfa_scolaireDepart/ajouterCoursForm.cs
@@ -6,5 +6,44 @@ namespace wfa_scolaireDepart
         {
             InitializeComponent();
         }
+
+        private bool TextBoxSontRemplis()
+        {
+            return (noCoursTextBox.Text != "" && nomCoursTextBox.Text != "" && ponderationTextBox.Text != "");
+        }
+
+        //private TblCour PrendreValeursTxtBox()
+        //{
+            
+        //}
+
+        private void ajouterButton_Click(object sender, EventArgs e)
+        {
+            //Cours cours = new Cours();
+            //ManagerCours managerCours = new ManagerCours();
+            //int nombreLignesAffectes = 0;
+            //try
+            //{
+            //    if (TextBoxSontRemplis())
+            //    {
+            //        //prendre des valeurs
+            //        cours = PrendreValeursTextBox();
+            //        //appeler fonction d'ajout
+            //        nombreLignesAffectes = managerCours.AjouterCours(cours);
+            //        if (nombreLignesAffectes > 0)
+            //        {
+            //            MessageBox.Show("Ajout avec succès.");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Entrez toutes les données.");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
     }
 }
